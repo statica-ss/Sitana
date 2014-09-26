6887c3e1da4c944ec3b8931a9b6bb921903289a1
Sebastian Sejud <sejud@statica.pl>
2014-09-26 16:34:38 +0200

    iOS Sample

--- Samples/TestApp/Main.Windows.cs
+++ Samples/TestApp/Main.Windows.cs
@@ -16,50 +16,6 @@ using Sitana.Framework.Diagnostics;
 
 namespace TestApp
 {
-    public class TestController: UiController
-    {
-        public TestController()
-        {
-            TextColor = new ColorWrapper();
-        }
-
-        void ChangeColors()
-        {
-            Random random = new Random();
-            while(true)
-            {
-                _color.Value = new Color(random.Next(255), random.Next(255), random.Next(255));
-                Thread.Sleep(500);
-            }
-        }
-
-        protected void OnClick(UiButton button)
-        {
-            Console.WriteLine("Test button");
-        }
-
-        public bool GetVisible(int val)
-        {
-            return val > 4;
-        }
-
-        public string Test(string text)
-        {
-            return text;
-        }
-
-        public StringBuilder Test2()
-        {
-            _text.Append("Another text got from controller.");
-            return _text;
-        }
-
-        public ColorWrapper TextColor { get; private set; }
-
-        ColorWrapper _color = new ColorWrapper(Color.Green);
-        StringBuilder _text = new StringBuilder();
-    }
-
     public static class Program
     {
         /// <summary>
@@ -68,8 +24,6 @@ namespace TestApp
         [STAThread]
         static void Main()
         {
-            string[] args = System.Environment.GetCommandLineArgs();
-
             using (var main = new AppMain())
             {
                 ContentLoader.Init(main.Content, "TestApp");
@@ -81,16 +35,11 @@ namespace TestApp
                 main.Graphics.IsFullScreen = false;
 
                 main.IsMouseVisible = true;
-                main.OnLoadContent += OnLoadContent;
+                main.OnLoadContent += TestController.OnLoadContent;
                 main.OnLoadedView += (a) => a.ResizeToView();
 
                 main.Run();
             }
-        }
-
-        static void OnLoadContent(AppMain main)
-        {
-            FontManager.Instance.AddFont("TestFont", "Fonts/TestFont", 16);
         }
     }
 }
---
+++ Samples/TestApp/Main.iOs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TestApp
+{
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		public override void FinishedLaunching(UIApplication app)
+		{
+			var main = new AppMain();
+
+			ContentLoader.Init(main.Content, "Assets");
+
+			main.LoadView("Ui/MainView");
+
+			main.Graphics.IsFullScreen = true;
+
+			main.OnLoadContent += TestController.OnLoadContent;
+
+			main.Run();
+		}
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	} 
+}
