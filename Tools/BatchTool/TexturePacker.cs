832fabfd20d39a6c6e7bdb54e0d112a6401ffb06
Sebastian Sejud <sejud@statica.pl>
2014-10-14 10:07:50 +0200

    ...

--- Tools/BatchTool/TexturePacker.cs
+++ Tools/BatchTool/TexturePacker.cs
@@ -17,6 +17,7 @@ namespace BatchTool
 
             int maxWidth;
             int margin;
+            bool crop;
 
             if (!int.TryParse(node.Attribute("MaxWidth"), out maxWidth))
             {
@@ -26,6 +27,11 @@ namespace BatchTool
             if (!int.TryParse(node.Attribute("Margin"), out margin))
             {
                 margin = 4;
+            }
+
+            if (!bool.TryParse(node.Attribute("Crop"), out crop))
+            {
+                crop = false;
             }
 
             string input = node.Attribute("Input");
@@ -44,8 +50,6 @@ namespace BatchTool
 
             string[] files = Directory.GetFiles(input, pattern, SearchOption.AllDirectories);
 
-            
-
             Bitmap tempBitmap = new Bitmap(maxWidth, 4096, PixelFormat.Format32bppArgb);
 
             List<Tuple<string, Image>> images = new List<Tuple<string, Image>>();
@@ -62,8 +66,13 @@ namespace BatchTool
                 {
                     string id = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
                     id = textureId.Replace("*", id.Substring(input.Length)).Replace('\\', '/').Replace("//", "/");
-                    Image image = Bitmap.FromFile(file);
-                    
+                    Bitmap image = (Bitmap)Bitmap.FromFile(file);
+
+                    if (crop)
+                    {
+                        image = Crop(image);
+                    }
+
                     Console.WriteLine("File: {0} = {1} x {2} -> {3}", file, image.Width, image.Height, id);
 
                     images.Add(new Tuple<string, Image>(id, image));
@@ -131,6 +140,106 @@ namespace BatchTool
                         writer.Write(tex.Item2.Height);
                     }
                 }
+            }
+        }
+
+        static Bitmap Crop(Bitmap bmp)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+
+            Func<int, bool> allWhiteRow = row =>
+            {
+                for (int i = 0; i < w; ++i)
+                    if (bmp.GetPixel(i, row).R != 255)
+                        return false;
+                return true;
+            };
+
+            Func<int, bool> allWhiteColumn = col =>
+            {
+                for (int i = 0; i < h; ++i)
+                    if (bmp.GetPixel(col, i).R != 255)
+                        return false;
+                return true;
+            };
+
+            int topmost = 0;
+            for (int row = 0; row < h; ++row)
+            {
+                if (allWhiteRow(row))
+                    topmost = row;
+                else break;
+            }
+
+            int bottommost = 0;
+            for (int row = h - 1; row >= 0; --row)
+            {
+                if (allWhiteRow(row))
+                    bottommost = row;
+                else break;
+            }
+
+            int leftmost = 0, rightmost = 0;
+            for (int col = 0; col < w; ++col)
+            {
+                if (allWhiteColumn(col))
+                    leftmost = col;
+                else
+                    break;
+            }
+
+            for (int col = w - 1; col >= 0; --col)
+            {
+                if (allWhiteColumn(col))
+                    rightmost = col;
+                else
+                    break;
+            }
+
+            rightmost = Math.Min(rightmost + 1, w);
+            leftmost = Math.Max(0, leftmost);
+            topmost = Math.Max(0, topmost);
+            bottommost = Math.Min(bottommost + 1, h);
+
+            if (rightmost == 0) rightmost = w; // As reached left
+            if (bottommost == 0) bottommost = h; // As reached top.
+
+            int croppedWidth = rightmost - leftmost;
+            int croppedHeight = bottommost - topmost;
+
+            if (croppedWidth == 0) // No border on left or right
+            {
+                leftmost = 0;
+                croppedWidth = w;
+            }
+
+            if (croppedHeight == 0) // No border on top or bottom
+            {
+                topmost = 0;
+                croppedHeight = h;
+            }
+
+            try
+            {
+                var target = new Bitmap(croppedWidth, croppedHeight);
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.Clear(Color.Aqua);
+                    g.DrawImage(bmp,
+                      new RectangleF(0, 0, croppedWidth, croppedHeight),
+                      new RectangleF(leftmost, topmost, croppedWidth, croppedHeight),
+                      GraphicsUnit.Pixel);
+
+                    bmp.Dispose();
+                }
+                return target;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(
+                  string.Format("Values are topmost={0} btm={1} left={2} right={3} croppedWidth={4} croppedHeight={5}", topmost, bottommost, leftmost, rightmost, croppedWidth, croppedHeight),
+                  ex);
             }
         }
     }
