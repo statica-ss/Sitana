49aae1ccf7f1143bd2f05ae63ad0f237f221629f
ebatiano <ebatiano@ebatianos.com>
2014-11-27 16:02:10 +0100

    Sprites

--- Sitana.Framework/Graphics/Sprites/Sprite.cs
+++ Sitana.Framework/Graphics/Sprites/Sprite.cs
@@ -2,23 +2,87 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Sitana.Framework.Content;
+using Sitana.Framework.Xml;
+using System.IO;
+using Microsoft.Xna.Framework;
 
 namespace Sitana.Framework.Graphics
 {
-    public class Sprite
+    public partial class Sprite
     {
-        Dictionary<string, List<PartialTexture2D>> _sequences = new Dictionary<string, List<PartialTexture2D>>();
+        public Point FrameSize { get; private set; }
+        Dictionary<string, Sequence> _sequences = new Dictionary<string, Sequence>();
 
         public static void LoadSprites(string name)
         {
+            XNode node = ContentLoader.Current.Load<XFile>(name);
 
+            if (node.Tag != "Sprites")
+            {
+                throw new InvalidDataException("Invalid node. Expected: Sprites.");
+            }
+
+            foreach (var cn in node.Nodes)
+            {
+                if (cn.Tag != "Sprite")
+                {
+                    throw new InvalidDataException("Invalid node. Expected: Sprite.");
+                }
+
+                Point frameSize = Point.Zero;
+
+                string spriteName = cn.Attribute("Name");
+                Dictionary<string, Sequence> sequences = new Dictionary<string,Sequence>();
+
+                foreach (var seq in cn.Nodes)
+                {
+                    if (seq.Tag != "Sequence")
+                    {
+                        throw new InvalidDataException("Invalid node. Expected: Sequence.");
+                    }
+
+                    Sequence sequence = new Sequence(seq);
+
+                    int width = sequence.Images[0].Width;
+                    int height = sequence.Images[0].Height;
+
+                    if (width != frameSize.X && frameSize.X != 0 || height != frameSize.Y && frameSize.Y != 0)
+                    {
+                        throw new InvalidDataException("Sprite must have same size frames.");
+                    }
+
+                    sequences.Add(sequence.Name, sequence);
+                }
+
+                var sprite = new Sprite()
+                {
+                    _sequences = sequences,
+                    FrameSize = frameSize
+                };
+
+                ContentLoader.Current.AddContent(spriteName, typeof(Sprite), sprite);
+            }
         }
 
-        SpriteInstance CreateInstance()
+        public Sequence FirstSequence
+        {
+            get
+            {
+                return _sequences.First().Value;
+            }
+        }
+
+        public SpriteInstance CreateInstance()
         {
             return new SpriteInstance(this);
         }
 
-        
+        public Sequence FindSequence(string name)
+        {
+            Sequence seq;
+            _sequences.TryGetValue(name, out seq);
+            return seq;
+        }
     }
 }
--- Sitana.Framework/Graphics/Sprites/SpriteInstance.cs
+++ Sitana.Framework/Graphics/Sprites/SpriteInstance.cs
@@ -2,16 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Sitana.Framework.Cs;
 
 namespace Sitana.Framework.Graphics
 {
     public class SpriteInstance
     {
         Sprite _sprite;
+        Sprite.Sequence _sequence = null;
+        float _frame = 0;
+        bool _animate = true;
+
+        public event EmptyArgsVoidDelegate AnimationFinished;
+        public event EmptyArgsVoidDelegate AnimationReplay;
+
+        public string Sequence
+        {
+            set
+            {
+                _sequence = _sprite.FindSequence(value);
+                _frame = 0;
+                _animate = true;
+            }
+
+            get
+            {
+                return _sequence.Name;
+            }
+        }
+
+        public PartialTexture2D FrameImage
+        {
+            get
+            {
+                return _sequence.Images[(int)_frame];
+            }
+        }
+
+        public int CurrentFrame
+        {
+            get
+            {
+                return (int)_frame;
+            }
+
+            set
+            {
+                _frame = value;
+            }
+        }
 
         internal SpriteInstance(Sprite sprite)
         {
             _sprite = sprite;
+            _sequence = _sprite.FirstSequence;
+        }
+
+        public void Animate(float time)
+        {
+            if (_animate)
+            {
+                float max = _sequence.Images.Count;
+                _frame += _sequence.Fps * time;
+
+                if (_frame >= max)
+                {
+                    if (_sequence.Loop)
+                    {
+                        _frame -= max;
+                        if (AnimationReplay != null)
+                        {
+                            AnimationReplay();
+                        }
+                    }
+                    else
+                    {
+                        _frame = max - 0.0001f;
+                        _animate = false;
+
+                        if (AnimationFinished != null)
+                        {
+                            AnimationFinished();
+                        }
+                    }
+                }
+            }
         }
     }
 }
